d652cec52bbc18f3afdd5411169635593e514fa3
Muhammad Sulaiman <gmmbbs000@gmail.com>
2024-10-25 15:41:46 +0500

    create sign in, sign up, forgot password pages and OAuth component and fixed a bug in header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,10 +4,12 @@ import { useLocation, useNavigate } from 'react-router'
 const Header = () => {
     const location = useLocation();
     const navigate = useNavigate()
-    console.log(location);
+    console.log(location.pathname);
+    
     function pathMatchRoute(route) {
         if(route === location.pathname) {
-            return true
+            console.log("ok")
+            return true;
         }
     }
 
@@ -15,18 +17,17 @@ const Header = () => {
     <div className='bg-white border-b shadow-sm sticky top-0 z-50'>
       <header className='flex justify-between items-center px-3 max-w-6xl mx-auto'>
         <div>
-            {/* <img width="142" height="46" alt="realtor.com"  src="https://static.rdc.moveaws.com/rdc-ui/logos/logo-brand.svg" /> */}
             <img className='h-5 cursor-pointer' alt="realtor.com"  src="https://static.rdc.moveaws.com/rdc-ui/logos/logo-brand.svg" onClick={() => navigate('/')} />
         </div>
         <div>
             <ul className='flex space-x-10'>
-                <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMatchRoute('/') && 'text-black border-b-red-500'}`} onClick={() => navigate('/')}>
+                <li className={`cursor-pointer py-3 text-sm font-semibold border-b-[3px] ${pathMatchRoute('/') ? "text-black border-b-red-500" : 'border-b-transparent text-gray-400'}`} onClick={() => navigate('/')}>
                     Home
                 </li>
-                <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMatchRoute('/offers') && 'text-black border-b-red-500'}`} onClick={() => navigate('/offers')}>
+                <li className={`cursor-pointer py-3 text-sm font-semibold border-b-[3px] ${pathMatchRoute('/offers') ? 'text-black border-b-red-500' : 'border-b-transparent text-gray-400'}`} onClick={() => navigate('/offers')}>
                     Offers
                 </li>
-                <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMatchRoute('/sign-in') && 'text-black border-b-red-500'}`} onClick={() => navigate('/sign-in')}>
+                <li className={`cursor-pointer py-3 text-sm font-semibold border-b-[3px] ${pathMatchRoute('/sign-in') ? 'text-black border-b-red-500' : 'border-b-transparent text-gray-400'}`} onClick={() => navigate('/sign-in')}>
                     Sign in
                 </li>
             </ul>
@@ -36,4 +37,4 @@ const Header = () => {
   )
 }
 
-export default Header
+export default Header;
--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -1,11 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { AiFillEyeInvisible, AiFillEye } from 'react-icons/ai';
+import { Link } from 'react-router-dom';
+import OAuth from '../components/OAuth';
 
-function ForgotPassword() {
+const ForgotPassword = () => {
+  const [email, setEmail] = useState("");
+
+  function onChange(e) {
+    setEmail(e.target.value);
+  }
+
   return (
-    <div>
-      Forgot Password
-    </div>
+    <section>
+      <h1 className='text-3xl text-center mt-6 font-bold'>Forgot Password</h1>
+
+      <div className='flex justify-center flex-wrap items-center px-6 py-12 max-w-6xl mx-auto'>
+        <div className='md:w-[67%] lg:w-[50%] mb-12 md:mb-6'>
+          <img className='w-full rounded-2xl' src="https://images.unsplash.com/flagged/photo-1564767609342-620cb19b2357?q=80&w=1373&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="key" />
+        </div>
+        <div className='w-full md:w-[67%] lg:w-[40%] lg:ml-20'>
+          <form >
+            <input 
+              className='mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out' 
+              type="email" 
+              id="email" 
+              value={email} 
+              onChange={onChange} 
+              placeholder='Email address'
+            />
+
+            <div className='flex justify-between whitespace-nowrap text-sm sm:text-lg'>
+              <p className='mb-6'>
+                Don't have an account?
+                <Link to="/sign-up" className='text-red-600 hover:text-red-700 transition duration-200 ease-in-out ml-1'>Register</Link>
+              </p>
+              <p>
+                <Link to="/sign-in" className='text-blue-600 hover:text-blue-800 transition duration-200 ease-in-out'>Sign in instead</Link>
+              </p>
+            </div>
+            <button type='submit' className='w-full bg-blue-600 text-white px-7 py-3 text-sm font-medium uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out' >
+              Send reset password
+            </button>
+            <div className='my-4 flex items-center before:border-t before:border-gray-300 before:flex-1 after:border-t after:border-gray-300 after:flex-1'>
+              <p className='text-center mx-4'>OR</p>
+            </div>
+            <OAuth />
+          </form>
+        </div>
+      </div>
+    </section>
   )
 }
 
-export default ForgotPassword
+export default ForgotPassword;
